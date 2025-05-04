827fa593f44cfdd0e678714780d62e6399fd140c
Varghese Alex <varghesealexa@gmail.com>
2025-05-05 01:32:03 +0530

    Episode 3 - Laying the foundation

--- App.js
+++ App.js
@@ -41,5 +41,34 @@ const parent = React.createElement("div", { id: "parent" }, [
   ),
 ]);
 
+const jsxHeading = <h1 id="heading">Hello World using React</h1>; // creates a js object
+
+const jsxParent = (
+  <div id="parent">
+    <div id="child1">
+      <h1>I am the first h1 tag</h1>
+    </div>
+    <div id="child2">
+      <h1>I am the second h1 tag</h1>
+    </div>
+  </div>
+);
+
+const TitleComponent = () => {
+  return <h1 id="title">Hello World using React Title</h1>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div id="parent">
+      <TitleComponent />
+      <TitleComponent></TitleComponent>
+      {TitleComponent()}
+      <h2 id="heading">Hello World using React Component</h2>
+      {jsxParent}
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
